dca92476c0c11ea4d6ca1e19bb3463d0d95c3af8
Michiel Idema <michielidema@outlook.com>
2025-03-24 21:10:23 +0100

    Debug styles

---
+++ app/styles/debug.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+export const debugStyles = StyleSheet.create({
+    container: {
+        borderBlockColor: "black", 
+        borderWidth: 2,
+        borderStyle: "dashed"
+    },
+    item: {
+        backgroundColor: "blue"
+    },
+})
